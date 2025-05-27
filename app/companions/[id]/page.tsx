878e1c9abde65071f11dc738f2fb201b606e59d2
robertWalker68501 <robert.walker6580@gmail.com>
2025-05-27 15:17:51 -0700

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,9 @@
+const CompanionSession = () => {
+  return (
+    <div>
+      <h1>Companion Session</h1>
+    </div>
+  );
+};
+
+export default CompanionSession;
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-semibold'>Sign In</h1>
+    </div>
+  );
+};
+
+export default SignInPage;
