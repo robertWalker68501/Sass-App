c82b5f05d592d43b36a398e3ee92db4afeccebdf
robertWalker68501 <robert.walker6580@gmail.com>
2025-05-27 16:18:07 -0700

    implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,30 @@
-import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
-import "./globals.css";
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { Bricolage_Grotesque } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
 
 const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
+  variable: '--font-bricolage',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
-  description: "Real-time AI Teaching Platform",
+  title: 'Converso',
+  description: 'Real-time AI Teaching Platform',
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
+    <html lang='en'>
+      <body className={`${bricolage.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import NavItems from '@/components/NavItems';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <Link href='/'>
+        <div className='flex cursor-pointer items-center gap-2.5'>
+          <Image
+            src='/images/logo.svg'
+            alt='logo'
+            width={46}
+            height={44}
+          />
+        </div>
+      </Link>
+      <div className='flex items-center gap-8'>
+        <NavItems />
+        <p>Sign In</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
